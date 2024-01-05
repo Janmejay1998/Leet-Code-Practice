53127b45a9c3398e11703ced92c1738887b317ef
Janmejay Mohanty <33492805+Janmejay1998@users.noreply.github.com>
2024-01-05 12:35:13 -0500

    Time: 47 ms (78.22%) | Memory: 17.4 MB (12.00%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.py
+++ 13-roman-to-integer/roman-to-integer.py
@@ -1,12 +1,21 @@
 class Solution:
     def romanToInt(self, s: str) -> int:
+        
+        # Making Dictionary of Roman to Number
         Roman_dict = {'I':1,'V':5,'X':10,'L':50,'C':100,'D':500,'M':1000}
-        val=0
-        for i in range(len(s)-1):
-            if Roman_dict[s[i]] < Roman_dict[s[i+1]]:
-                val = val - Roman_dict[s[i]]
-                print(val)
+
+        number = 0    #Initializing number variable
+
+        for i in range(len(s)-1):          #Preventing loop from getting index out of bound
+
+            # Checking if current Roman value is less than next Roman value
+
+            if Roman_dict[s[i]] < Roman_dict[s[i+1]]: 
+                number = number - Roman_dict[s[i]]
+
             else:
-                val = val + Roman_dict[s[i]]
-        val = val + Roman_dict[s[-1]]
-        return val
+                number = number + Roman_dict[s[i]]
+        
+        number = number + Roman_dict[s[-1]]      # Adding last remaining element of the String
+
+        return number
