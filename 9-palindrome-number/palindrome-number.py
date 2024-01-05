a0f5a8cf828c18a6764c6f9bcc28c18c9ea717e2
Janmejay Mohanty <33492805+Janmejay1998@users.noreply.github.com>
2024-01-05 10:03:27 -0500

    Time: 68 ms (33.33%) | Memory: 17.4 MB (9.24%) - LeetSync

--- 9-palindrome-number/palindrome-number.py
+++ 9-palindrome-number/palindrome-number.py
@@ -1,13 +1,16 @@
 class Solution:
     def isPalindrome(self, x: int) -> bool:
-        if x < 0:
+        if x < 0:           # Checking value is negative or positive 
             return False
-        num = x
-        rev = 0
+
+        num = x             # Make a copy of original value
+        rev = 0             # Assigning reverse variable to zero
+
         while x!=0:
-            r = x % 10
-            rev = rev * 10 + r 
-            x = int(x / 10)
-        if rev == num:
+            r = x % 10              # Storing remainder 
+            rev = rev * 10 + r      # Reversing the value
+            x = int(x / 10)         # Updating X value with each iteration
+        
+        if rev == num:      # Palindrome checking 
             return True
             
