c1e41fe0174426c4639b149b9d8bd4c31fb14427
Janmejay Mohanty <33492805+Janmejay1998@users.noreply.github.com>
2024-01-06 16:04:07 -0500

    Time: 34 ms (84.17%) | Memory: 17.2 MB (25.98%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.py
+++ 20-valid-parentheses/valid-parentheses.py
@@ -3,13 +3,15 @@ class Solution:
 
         stack = []
         
-        par_dict = {'(':')','{':'}','[':']'}
+        par_dict = {'(':')','{':'}','[':']'}    # Paranthesis Dictionary
 
-        for ele in s:
-            if ele in par_dict:
-                stack.append(par_dict[ele])
+        for ele in s:       
+            if ele in par_dict:         # If opening bracket is in dictionary then append in Stack
+                stack.append(par_dict[ele])        
 
-            elif not stack or ele != stack.pop():
+            #If there is no opening bracket and current bracket not equal to Stack
+
+            elif not stack or ele != stack.pop():    
                 return False
 
         # If Stack is empty and size of String is even    
