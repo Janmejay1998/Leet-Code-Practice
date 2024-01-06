08613de92e72084992747b001d7a7750c379dda0
Janmejay Mohanty <33492805+Janmejay1998@users.noreply.github.com>
2024-01-05 19:09:44 -0500

    Time: 24 ms (99.77%) | Memory: 17.4 MB (18.70%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.py
+++ 14-longest-common-prefix/longest-common-prefix.py
@@ -1,19 +1,23 @@
 class Solution:
     def longestCommonPrefix(self, strs: List[str]) -> str:
-        res=""
-        if "" in strs:
-            return ""
-
-        if len(strs) == 1:
-            return strs[0]
         
+        lcp=""
+
+        if "" in strs:         # Checking if list have [""]
+            return ""           # Returning ""
+
+        if len(strs) == 1:      # Checking if list have single element
+            return strs[0]      # Returning that single element
+
         else:
-            strs.sort()
-            for i in range(len(strs[0])):
-                if strs[0][i] != strs[-1][i]:
-                    return res
-                res += strs[0][i]      
-        return res
+            strs.sort()         # Sorting in Alphabetically order
+
+            for i in range(len(strs[0])):       # Iterating on first element of sorted list
+                if strs[0][i] != strs[-1][i]:   # Comparing first and last element of sorted list
+                    return lcp
+                lcp += strs[0][i]           #Storing Longest Common Prefix
+
+        return lcp
             
 
         
