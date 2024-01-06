1fb5ed556f6903eedb991b01555bdc94389f6140
Janmejay Mohanty <33492805+Janmejay1998@users.noreply.github.com>
2024-01-06 15:52:04 -0500

    Time: 43 ms (31.41%) | Memory: 17.4 MB (5.78%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.py
+++ 20-valid-parentheses/valid-parentheses.py
@@ -5,16 +5,13 @@ class Solution:
         
         brac = {'(':')','{':'}','[':']'}
 
-        if len(s)%2 == 1:
-            return False
-
         for ele in s:
             if ele in brac:
                 stack.append(brac[ele])
             elif not stack or ele != stack.pop():
                 return False
             
-        return not stack
+        return not stack and len(s)%2 == 0
             
         
         
