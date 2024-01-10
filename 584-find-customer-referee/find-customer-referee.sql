3cafc21393d5f0723ac9e00c2950e42e2ffed811
Janmejay Mohanty <33492805+Janmejay1998@users.noreply.github.com>
2024-01-10 15:33:59 -0500

    Time: 1103 ms (21.07%) | Memory: 0B (100.00%) - LeetSync

--- 584-find-customer-referee/find-customer-referee.sql
+++ 584-find-customer-referee/find-customer-referee.sql
@@ -1,4 +1,4 @@
 # Write your MySQL query statement below
 SELECT name
 FROM Customer
-WHERE COALESCE(referee_id,0) <> 2;     # COALESCE used for replacing null value with 0
+WHERE COALESCE(referee_id,0) <> 2;
