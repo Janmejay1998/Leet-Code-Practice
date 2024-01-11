2f65723f288e8ed080b8340cca7c86a8955363f4
Janmejay Mohanty <33492805+Janmejay1998@users.noreply.github.com>
2024-01-11 12:10:23 -0500

    Time: 1716 ms (17.86%) | Memory: 0B (100.00%) - LeetSync

--- 1390-average-selling-price/average-selling-price.sql
+++ 1390-average-selling-price/average-selling-price.sql
@@ -1,4 +1,5 @@
 # Write your MySQL query statement below
+# Using COALESCE to replace null with O value
 SELECT P.product_id, COALESCE(ROUND(SUM(P.price*U.units)/SUM(U.units),2),0) AS average_price
 FROM Prices P
 LEFT JOIN UnitsSold U ON U.product_id = P.product_id AND U.purchase_date BETWEEN P.start_date AND P.end_date
